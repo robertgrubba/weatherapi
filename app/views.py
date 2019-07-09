12cd7b50775589f5b3deed682793a8ff6c6995a6
Robert Grubba <rgrubba@gmail.com>
2019-07-09 13:06:14 +0200

    first version of windrose hotspots generator

--- app/views.py
+++ app/views.py
@@ -5,6 +5,7 @@ import urllib, json, urllib2, os, sys, ssl, hashlib, time,re
 from datetime import datetime
 from lxml import html
 from sqlalchemy import desc
+from PIL import Image, ImageDraw
 
 @app.route('/')
 def homepage():
@@ -66,9 +67,6 @@ def getSpot(name):
                 links = []
                 for item in hasLinks:
                     links.append(item.link)
-
-
-
             spotName = query.spot_name
             spdMin = query.spd_min
             spdMax = query.spd_max
@@ -90,3 +88,35 @@ def getSpot(name):
                 status =200
                 )
 
+@app.route('/windrose/<string:name>')
+def drawWindRose(name):
+    GoogleMapsAPIKey = os.environ['GOOGLEMAPSAPI']
+    if len(name)<3:
+        return jsonify(
+                status=400
+                )
+    else:
+	    results = db.session.query(Spots).filter_by(spot_name = name)
+	    if results is None:
+		return jsonify(
+			status=404
+			)
+	    else:
+		dirMin = []
+		dirMax = []
+		for result in results:
+			dirMin.append(result.dir_min)
+			dirMax.append(result.dir_max)
+			lat = result.lat
+			lon = result.lon
+		googleimg = urllib.urlopen("https://maps.googleapis.com/maps/api/staticmap?center="+str(lat)+","+str(lon)+"&maptype=satellite&scale=2&zoom=16&size=150x150&key="+GoogleMapsAPIKey)
+		img = Image.open(googleimg).convert('RGBA')
+		windrose = Image.new('RGBA',(300,300),(255,255,255,0))
+		draw = ImageDraw.Draw(windrose)
+		numberOfResults = results.count()
+		for size in range(0,140):
+			for x in range(numberOfResults):
+				draw.arc([20+size,20+size,280-size,280-size],dirMin[x],dirMax[x],fill=(0,255,0,90))
+		composite = Image.alpha_composite(img, windrose.rotate(90))
+#		composite.save(str(name)+".png", "PNG")
+		return composite
